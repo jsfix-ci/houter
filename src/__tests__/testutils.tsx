cb80f25dd500547b10bae73a85d359d951a74ca9
ltm <lintuming@tuzhanai.com>
2019-09-10 15:26:20 +0800

    style (test): prettier

--- src/__tests__/testutils.tsx
+++ src/__tests__/testutils.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from "react";
-import { Router } from "../Router";
-import { createBrowserHistory, createHashHistory } from "history";
-import { mount } from "enzyme";
+import React, { ReactNode } from 'react';
+import { Router } from '../Router';
+import { createBrowserHistory, createHashHistory } from 'history';
+import { mount } from 'enzyme';
 const mountInBrowserRouter = (jsx: ReactNode) => {
   const history = createBrowserHistory();
   const wrapper = mount(<Router history={history}>{jsx}</Router>);
